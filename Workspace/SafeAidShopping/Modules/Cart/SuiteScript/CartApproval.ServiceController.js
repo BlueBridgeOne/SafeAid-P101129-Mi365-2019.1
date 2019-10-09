923f6fdfd9db0e91cf143273161794b120f6ea04
GTruslove <gordontruslove@hotmail.com>
2019-10-09 16:08:02 +0100

    started working on approving orders.

--- Workspace/SafeAidShopping/Modules/Cart/SuiteScript/CartApproval.ServiceController.js
+++ Workspace/SafeAidShopping/Modules/Cart/SuiteScript/CartApproval.ServiceController.js
@@ -20,7 +20,7 @@ define(
 
 				var buyer = this.getBuyer(contact);
 				var warnings = [];
-				var items = order.getItems(["internalid", "quantity", "options", "displayname", "amount", "amount_formatted","parent"]);
+				var items = order.getItems(["internalid", "quantity", "options", "displayname", "amount", "amount_formatted", "parent"]);
 				var options, wearerChecks = [],
 					areaChecks = []; //Get a list of the checks that are needed.
 				var areas = [],
@@ -83,10 +83,12 @@ define(
 				//Note: somewhere down the line there will need to be a currency conversion done on all the values, to get back to GBP.
 				var currency = session.getShopperCurrency();
 
-				var testing=false; //Force lots of warnings
+				var testing = false; //Force lots of warnings
 				//Go through all the rules and check them.
 				//Buyer Budget!
-				if (testing||buyer.custentity_bb1_sca_currentspend + summary.total > buyer.custentity_bb1_sca_budget) {
+				var hwarnings={};
+				if (testing || buyer.custentity_bb1_sca_currentspend + summary.total > buyer.custentity_bb1_sca_budget) {
+					if(this.checkDuplicateWarning(hwarnings,"WARNING_BUYER_BUDGET")){
 					warnings.push({
 						message: "WARNING_BUYER_BUDGET",
 						values: {
@@ -95,15 +97,18 @@ define(
 						}
 					});
 				}
+				}
 				//Area Budget!
 				var check, area;
 				var areaDetails = this.getAreas(areas);
-				var wearerDetails = this.getWearers(areas);
+				var wearerDetails = this.getWearers(wearers);
 				var ruleDetails = this.getRules(areas);
+				
 				for (var i = 0; i < areaChecks.length; i++) {
 					check = areaChecks[i];
 					area = areaDetails[check.area.value];
-					if (testing||area.custrecord_bb1_sca_area_currentspend + summary.total > area.custrecord_bb1_sca_area_budget) {
+					if (testing || area.custrecord_bb1_sca_area_currentspend + summary.total > area.custrecord_bb1_sca_area_budget) {
+						if(this.checkDuplicateWarning(hwarnings,"WARNING_AREA_BUDGET,"+check.area.value)){
 						warnings.push({
 							message: "WARNING_AREA_BUDGET",
 							values: {
@@ -113,65 +118,84 @@ define(
 							}
 						});
 					}
+					}
 				}
+				//
 				for (var i = 0; i < wearerChecks.length; i++) {
 					check = wearerChecks[i];
 					wearer = wearerDetails[check.wearer.value];
-					if (testing||wearer.custrecord_bb1_sca_wearer_currentspend + summary.total > wearer.custrecord_bb1_sca_wearer_budget) {
-						warnings.push({
-							message: "WARNING_WEARER_BUDGET",
-							values: {
-								wearer: check.wearer,
-								budget: wearer.custrecord_bb1_sca_wearer_currentspend,
-								duration: wearer.custrecord_bb1_sca_wearer_duration
-							}
-						});
+					if (wearer) {
+						if (testing || wearer.custrecord_bb1_sca_wearer_currentspend + summary.total > wearer.custrecord_bb1_sca_wearer_budget) {
+							if(this.checkDuplicateWarning(hwarnings,"WARNING_WEARER_BUDGET,"+check.wearer.value)){
+							warnings.push({
+								message: "WARNING_WEARER_BUDGET",
+								values: {
+									wearer: check.wearer,
+									budget: wearer.custrecord_bb1_sca_wearer_currentspend,
+									duration: wearer.custrecord_bb1_sca_wearer_duration
+								}
+							});
+						}
+						}
 					}
+					// else{
+					// 	throw(new Error("Wearer not found! "+i+" "+JSON.stringify(check)+" check.wearer.value "+check.wearer.value+" "+JSON.stringify(wearerDetails)+" "+JSON.stringify(wearerChecks)));
+					// }
 				}
 				var item, rule;
 				for (var i = 0; i < items.length; i++) {
 					item = items[i];
 					area = null;
 					wearer = null;
-					if(item.options){
-					for (var k = 0; k < item.options.length; k++) {
-						if (item.options[k].id == "CUSTCOL_BB1_SCA_AREA") {
-							area = item.options[k].value;
-						}
-						if (item.options[k].id == "CUSTCOL_BB1_SCA_WEARER") {
-							wearer = item.options[k].value;
+					if (item.options) {
+						for (var k = 0; k < item.options.length; k++) {
+							if (item.options[k].id == "CUSTCOL_BB1_SCA_AREA") {
+								area = item.options[k].value;
+							}
+							if (item.options[k].id == "CUSTCOL_BB1_SCA_WEARER") {
+								wearer = item.options[k].value;
+							}
 						}
 					}
-				}
 					if (area) {
 						for (var j = 0; j < ruleDetails.length; j++) {
 							rule = ruleDetails[j];
-							if (rule.custrecord_bb1_sca_rule_area.value == area && (rule.custrecord_bb1_sca_rule_item == item.internalid||rule.custrecord_bb1_sca_rule_item == item.parentid)) {
+							if (rule.custrecord_bb1_sca_rule_area.value == area && (rule.custrecord_bb1_sca_rule_item == item.internalid || rule.custrecord_bb1_sca_rule_item == item.parentid)) {
 								if (rule.custrecord_bb1_sca_rule_wearer && rule.custrecord_bb1_sca_rule_wearer.value != "") {
 									if (rule.custrecord_bb1_sca_rule_wearer.value == wearer) {
-										if (testing||rule.custrecord_bb1_sca_rule_purchased + item.quantity > rule.custrecord_bb1_sca_rule_quantity) {
-										warnings.push({
-											message: "WARNING_RULE_WEARER_MAX",
-											values: {
-												item:{value:item.intenalid,text:item.displayname},
-												wearer: rule.custrecord_bb1_sca_rule_wearer,
-												max: rule.custrecord_bb1_sca_rule_quantity,
-												duration:rule.custrecord_bb1_sca_rule_duration
-											}
-										});
-									}
+										if (testing || rule.custrecord_bb1_sca_rule_purchased + item.quantity > rule.custrecord_bb1_sca_rule_quantity) {
+											if(this.checkDuplicateWarning(hwarnings,"WARNING_RULE_WEARER_MAX,"+item.intenalid+","+rule.custrecord_bb1_sca_rule_wearer.value)){
+											warnings.push({
+												message: "WARNING_RULE_WEARER_MAX",
+												values: {
+													item: {
+														value: item.intenalid,
+														text: item.displayname
+													},
+													wearer: rule.custrecord_bb1_sca_rule_wearer,
+													max: rule.custrecord_bb1_sca_rule_quantity,
+													duration: rule.custrecord_bb1_sca_rule_duration
+												}
+											});
+										}
+										}
 									}
 								} else {
-									if (testing||rule.custrecord_bb1_sca_rule_purchased + item.quantity > rule.custrecord_bb1_sca_rule_quantity) {
+									if (testing || rule.custrecord_bb1_sca_rule_purchased + item.quantity > rule.custrecord_bb1_sca_rule_quantity) {
+										if(this.checkDuplicateWarning(hwarnings,"WARNING_RULE_AREA_MAX,"+item.intenalid+","+rule.custrecord_bb1_sca_rule_area.value)){
 										warnings.push({
 											message: "WARNING_RULE_AREA_MAX",
 											values: {
-												item:{value:item.intenalid,text:item.displayname},
+												item: {
+													value: item.intenalid,
+													text: item.displayname
+												},
 												area: rule.custrecord_bb1_sca_rule_area,
 												max: rule.custrecord_bb1_sca_rule_quantity,
-												duration:rule.custrecord_bb1_sca_rule_duration
+												duration: rule.custrecord_bb1_sca_rule_duration
 											}
 										});
+									}
 									}
 								}
 							}
@@ -182,29 +206,36 @@ define(
 
 				var result = {
 					success: true,
-					warnings: warnings,
-					items: items,
-					ruleDetails: ruleDetails
+					warnings: warnings
 				};
 
-				
-					// summary: summary,
-					// 
-					// currency: currency,
-					// buyer: buyer,
-					// areaChecks: areaChecks,
-					// wearerChecks: wearerChecks,
-					// areas: areas,
-					// wearers: wearers,
-					// areaDetails: areaDetails,
-					// wearerDetails: wearerDetails,
-					// ruleDetails: ruleDetails
+
+				// summary: summary,
+				// 
+				// currency: currency,
+				// buyer: buyer,
+				// areaChecks: areaChecks,
+				// wearerChecks: wearerChecks,
+				// areas: areas,
+				// wearers: wearers,
+				// areaDetails: areaDetails,
+				// wearerDetails: wearerDetails,
+				// ruleDetails: ruleDetails
 
 
 				var customer = context.getUser();
 
 				return result;
 			},
+			checkDuplicateWarning:function(hwarnings,warningid){
+				if(hwarnings[warningid]){
+					return false;
+				}else{
+					hwarnings[warningid]=true;
+					return true;
+				}
+			}
+			,
 			getWearers: function (wearers) {
 				if (wearers.length == 0) {
 					return {};
@@ -242,7 +273,7 @@ define(
 						var custrecord_bb1_sca_wearer_startdate = wearer.custrecord_bb1_sca_wearer_startdate;
 						wearer.custrecord_bb1_sca_wearer_budget = parseFloat(wearer.custrecord_bb1_sca_wearer_budget) || 0;
 						if (custrecord_bb1_sca_wearer_startdate == "") {
-							wearer.custrecord_bb1_sca_wearer_startdate = this.getStartDate(new Date(),wearer.custrecord_bb1_sca_wearer_duration);
+							wearer.custrecord_bb1_sca_wearer_startdate = this.getStartDate(new Date(), wearer.custrecord_bb1_sca_wearer_duration);
 							wearer.custrecord_bb1_sca_wearer_currentspend = 0;
 							nlapiSubmitField("customrecord_bb1_sca_wearer", result.getId(), "custrecord_bb1_sca_wearer_startdate", wearer.custrecord_bb1_sca_wearer_startdate, false);
 							nlapiSubmitField("customrecord_bb1_sca_wearer", result.getId(), "custrecord_bb1_sca_wearer_currentspend", wearer.custrecord_bb1_sca_wearer_currentspend, false);
@@ -251,10 +282,10 @@ define(
 							wearer.custrecord_bb1_sca_wearer_currentspend = parseFloat(wearer.custrecord_bb1_sca_wearer_currentspend);
 						}
 						var today = new Date();
-						var enddate = this.getEndDate(wearer.custrecord_bb1_sca_wearer_startdate,wearer.custrecord_bb1_sca_wearer_duration);
-						
+						var enddate = this.getEndDate(wearer.custrecord_bb1_sca_wearer_startdate, wearer.custrecord_bb1_sca_wearer_duration);
+
 						if (today > enddate) { //Start new duration
-							wearer.custrecord_bb1_sca_wearer_startdate = this.getStartDate(new Date(),wearer.custrecord_bb1_sca_wearer_duration);
+							wearer.custrecord_bb1_sca_wearer_startdate = this.getStartDate(new Date(), wearer.custrecord_bb1_sca_wearer_duration);
 							wearer.custrecord_bb1_sca_wearer_currentspend = 0;
 							nlapiSubmitField("customrecord_bb1_sca_wearer", result.getId(), "custrecord_bb1_sca_wearer_startdate", wearer.custrecord_bb1_sca_wearer_startdate, false);
 							nlapiSubmitField("customrecord_bb1_sca_wearer", result.getId(), "custrecord_bb1_sca_wearer_currentspend", wearer.custrecord_bb1_sca_wearer_currentspend, false);
@@ -301,7 +332,7 @@ define(
 						var custrecord_bb1_sca_area_startdate = area.custrecord_bb1_sca_area_startdate;
 						area.custrecord_bb1_sca_area_budget = parseFloat(area.custrecord_bb1_sca_area_budget) || 0;
 						if (custrecord_bb1_sca_area_startdate == "") {
-							area.custrecord_bb1_sca_area_startdate = this.getStartDate(new Date(),area.custrecord_bb1_sca_area_duration);
+							area.custrecord_bb1_sca_area_startdate = this.getStartDate(new Date(), area.custrecord_bb1_sca_area_duration);
 							area.custrecord_bb1_sca_area_currentspend = 0;
 							nlapiSubmitField("customrecord_bb1_sca_area", result.getId(), "custrecord_bb1_sca_area_startdate", area.custrecord_bb1_sca_area_startdate, false);
 							nlapiSubmitField("customrecord_bb1_sca_area", result.getId(), "custrecord_bb1_sca_area_currentspend", area.custrecord_bb1_sca_area_currentspend, false);
@@ -309,10 +340,10 @@ define(
 							area.custrecord_bb1_sca_area_startdate = new Date(area.custrecord_bb1_sca_area_startdate);
 							area.custrecord_bb1_sca_area_currentspend = parseFloat(area.custrecord_bb1_sca_area_currentspend);
 						}
-						var enddate = this.getEndDate(area.custrecord_bb1_sca_area_startdate,area.custrecord_bb1_sca_area_duration);
-						var today=new Date();
+						var enddate = this.getEndDate(area.custrecord_bb1_sca_area_startdate, area.custrecord_bb1_sca_area_duration);
+						var today = new Date();
 						if (today >= enddate) { //Start new duration
-							area.custrecord_bb1_sca_area_startdate = this.getStartDate(new Date(),area.custrecord_bb1_sca_area_duration);
+							area.custrecord_bb1_sca_area_startdate = this.getStartDate(new Date(), area.custrecord_bb1_sca_area_duration);
 							area.custrecord_bb1_sca_area_currentspend = 0;
 							nlapiSubmitField("customrecord_bb1_sca_area", result.getId(), "custrecord_bb1_sca_area_startdate", area.custrecord_bb1_sca_area_startdate, false);
 							nlapiSubmitField("customrecord_bb1_sca_area", result.getId(), "custrecord_bb1_sca_area_currentspend", area.custrecord_bb1_sca_area_currentspend, false);
@@ -321,28 +352,29 @@ define(
 					}
 				}
 				return results;
-			},getStartDate:function(date,duration){
+			},
+			getStartDate: function (date, duration) {
 				switch (duration) {
 					case "2": //quarter
-					return new Date(date.getFullYear(), 1+(4*Math.floor((date.getMonth()-1)/4)), 1);
+						return new Date(date.getFullYear(), 1 + (4 * Math.floor((date.getMonth() - 1) / 4)), 1);
 						break;
 					case "3": //year
-					return new Date(date.getFullYear(), 1, 1);
+						return new Date(date.getFullYear(), 1, 1);
 						break;
 				}
 				return new Date(date.getFullYear(), date.getMonth(), 1); //month
-			},getEndDate:function(date,duration){
+			},
+			getEndDate: function (date, duration) {
 				switch (duration) {
 					case "2": //quarter
-					return date.setMonth(date.getMonth() + 3);
+						return date.setMonth(date.getMonth() + 3);
 						break;
 					case "3": //year
-					return date.setMonth(date.getMonth() + 12);
+						return date.setMonth(date.getMonth() + 12);
 						break;
 				}
 				return date.setMonth(date.getMonth() + 1);
-			}
-			,
+			},
 			getBuyer: function (contact) {
 				if (!(contact > 0)) {
 					return;
@@ -353,7 +385,7 @@ define(
 					var custentity_bb1_sca_startdate = buyer.custentity_bb1_sca_startdate;
 					buyer.custentity_bb1_sca_budget = parseFloat(buyer.custentity_bb1_sca_budget) || 0;
 					if (custentity_bb1_sca_startdate == "") {
-						buyer.custentity_bb1_sca_startdate = this.getStartDate(new Date(),buyer.custentity_bb1_sca_duration.duration);
+						buyer.custentity_bb1_sca_startdate = this.getStartDate(new Date(), buyer.custentity_bb1_sca_duration.duration);
 						buyer.custentity_bb1_sca_currentspend = 0;
 						nlapiSubmitField("contact", contact, "custentity_bb1_sca_startdate", buyer.custentity_bb1_sca_startdate, false);
 						nlapiSubmitField("contact", contact, "custentity_bb1_sca_currentspend", buyer.custentity_bb1_sca_currentspend, false);
@@ -361,11 +393,11 @@ define(
 						buyer.custentity_bb1_sca_startdate = new Date(buyer.custentity_bb1_sca_startdate);
 						buyer.custentity_bb1_sca_currentspend = parseFloat(buyer.custentity_bb1_sca_currentspend);
 					}
-					var enddate = this.getEndDate(buyer.custentity_bb1_sca_startdate,buyer.custentity_bb1_sca_duration),
+					var enddate = this.getEndDate(buyer.custentity_bb1_sca_startdate, buyer.custentity_bb1_sca_duration),
 						today = new Date();
-					
+
 					if (today > enddate) { //Start new duration
-						buyer.custentity_bb1_sca_startdate = this.getStartDate(new Date(),buyer.custentity_bb1_sca_duration.duration);
+						buyer.custentity_bb1_sca_startdate = this.getStartDate(new Date(), buyer.custentity_bb1_sca_duration.duration);
 						buyer.custentity_bb1_sca_currentspend = 0;
 						nlapiSubmitField("contact", contact, "custentity_bb1_sca_startdate", buyer.custentity_bb1_sca_startdate, false);
 						nlapiSubmitField("contact", contact, "custentity_bb1_sca_currentspend", buyer.custentity_bb1_sca_currentspend, false);
--- Workspace/bb1/Modules/Mi365Main/JavaScript/SafeAid.bb1.Mi365Main.js
+++ Workspace/bb1/Modules/Mi365Main/JavaScript/SafeAid.bb1.Mi365Main.js
@@ -2,20 +2,78 @@
 
 define(
 	'SafeAid.bb1.Mi365Main', ['Handlebars',
-		 'SafeAid.bb1.Mi365Router'
+		'SafeAid.bb1.Mi365Router', 'OrderHistory.Details.View'
 	],
 	function (
-		Handlebars,Mi365Router
+		Handlebars, Mi365Router, OrderHistoryDetails
 	) {
 		'use strict';
 
-		Handlebars.registerHelper('toJSON', function(obj) {
+		Handlebars.registerHelper('toJSON', function (obj) {
 			return JSON.stringify(obj, null, 3);
 		});
 
 		return {
 			mountToApp: function mountToApp(container) {
-				
+
+				//show approval info on order history.
+				_.extend(OrderHistoryDetails.prototype, {
+
+					getContext: _.wrap(OrderHistoryDetails.prototype.getContext, function (getContext, options) {
+						var res = getContext.apply(this, _.rest(arguments));
+						console.log(res);
+						var options = res.model.get("options");
+						//console.log(options);
+						if (options.custbody_bb1_sca_approvalstatus == "3") {
+							res.showApproved = true;
+						} else if (options.custbody_bb1_sca_approvalstatus == "2") {
+							res.approvalRequired = true;
+							var warnings = [];
+							if (options.custbody_bb1_sca_approvaldata) {
+								console.log(options.custbody_bb1_sca_approvaldata);
+								var custbody_bb1_sca_approvaldata = JSON.parse(options.custbody_bb1_sca_approvaldata);
+								console.log(custbody_bb1_sca_approvaldata.warnings);
+								warnings = custbody_bb1_sca_approvaldata.warnings || []
+
+								var warning;
+								var durations = {
+									"1": "month",
+									"2": "quarter",
+									"3": "year"
+								};
+								//console.log(warnings);
+								for (var i = 0; i < warnings.length; i++) {
+									warning = warnings[i];
+									values = warning.values;
+									switch (warning.message) {
+										case "WARNING_BUYER_BUDGET":
+											warning.text = "The order exceeds your budget of £" + values.budget + " for this " + durations[values.duration] + ".";
+											break;
+										case "WARNING_AREA_BUDGET":
+											warning.text = "The order exceeds " + values.area.text + "'s budget of £" + values.budget + " for this " + durations[values.duration] + ".";
+											break;
+										case "WARNING_WEARER_BUDGET":
+											warning.text = "The order exceeds " + values.wearer.text + "'s budget of £" + values.budget + " for this " + durations[values.duration] + ".";
+											break;
+										case "WARNING_RULE_AREA_MAX":
+											warning.text = "Only " + values.max + " " + values.item.text + "'s can be purchased for " + values.area.text + "'s during this " + durations[values.duration] + ".";
+											break;
+										case "WARNING_RULE_WEARER_MAX":
+											warning.text = "Only " + values.max + " " + values.item.text + "'s can be purchased for " + values.wearer.text + " during this " + durations[values.duration] + ".";
+											break;
+									}
+								}
+
+							}
+							res.warnings=warnings;
+							console.log(warnings);
+						}
+						return res;
+					})
+				});
+
+
+
 				var layout = container.getComponent('Layout');
 
 				if (layout) {
