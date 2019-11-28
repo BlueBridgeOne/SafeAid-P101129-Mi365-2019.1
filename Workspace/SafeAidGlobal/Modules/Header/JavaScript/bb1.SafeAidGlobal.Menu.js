02944626e5a2eb10690656dff857019f2f768823
Michael Mostratos <mmostratos@bluebridgeone.com>
2019-11-29 02:47:13 +1000

    Move menu changes to global module

---
+++ Workspace/SafeAidGlobal/Modules/Header/JavaScript/bb1.SafeAidGlobal.Menu.js
@@ -0,0 +1,62 @@
+//@module bb1.SafeAidGlobal.Menu
+define(
+ 'bb1.SafeAidGlobal.Menu',
+ [
+  'Profile.Model',
+  'SC.Configuration',
+  'underscore',
+  'Utils'
+ ],
+ function (
+  ProfileModel,
+  Configuration,
+  _,
+  Utils
+ )
+{
+ 'use strict';
+
+ return {
+
+  mountToApp: function (container)
+  {
+   var layout = container.getComponent('Layout'),
+       profile = ProfileModel.getInstance(),
+       contactIsBuyer = profile.get('contactIsBuyer');
+   
+   if (contactIsBuyer) {
+     
+    var customerShowStandardItems = profile.get('customerShowStandardItems'),
+        contactOverrideCustomerItems = profile.get('contactOverrideCustomerItems'),
+        contactShowStandardItems = profile.get('contactShowStandardItems');
+    
+    if (!customerShowStandardItems || (contactOverrideCustomerItems && !contactShowStandardItems)) {
+     var allowedMenuItems = ['Home', 'Information'];
+     
+     Configuration.navigationData = _.filter(Configuration.navigationData, function(menuItem) {
+      return allowedMenuItems.indexOf(menuItem.text) != -1;
+     });
+    }
+    
+    Configuration.navigationData.push({
+     id: '',
+     parentId: '',
+     text: 'My Catalogue',
+     href: '/',
+     dataHashtag: '#/my-catalogue',
+     dataTouchpoint: 'home',
+     'data-hashtag': '#/my-catalogue',
+     'data-touchpoint': 'home',
+     class: 'header-menu-level1-anchor header-menu-mycatalogue-anchor',
+     classnames: 'header-menu-mycatalogue-anchor',
+     level: '1',
+     placeholder: ''
+    });
+    
+   }
+   
+  }
+
+ };
+ 
+});
